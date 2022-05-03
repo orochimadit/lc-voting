b97d65750116cb25350ca9ff14116576483dbaaf
Aditya Pratama Badra <orochimadit@gmail.com>
2022-05-03 20:52:16 +0700

    episode 4 the idea page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,50 @@
         </header>
         <main class="container mx-auto max-w-custom flex" style="max-width:1000px">
             <div class="w-70 mr-5" >
-           Add idea goes here. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                <div 
+                class="bg-white order-2 border-blue rounded-xl mt-16"
+                style="
+                            border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                            border-image-slice: 1;
+                            background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                            background-origin: border-box;
+                            background-clip: content-box, border-box;
+                "
+                >
+                <div class="text-center px-6 py-2 pt-6">
+                    <h3 class="font-semibold text-base">Add an idea</h3>
+                    <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!!</p>
+                </div>
+                <form action="" method="post" class="space-y-4 px-4 py-6">
+                    <div>
+                        <input type="text" name="" id="" class="w-full text-sm  bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
+                    </div>
+                    <div>
+                    <select name="category_add" id="category" class="w-full rounded-xl bg-gray-100 text-sm border-none px-4 py-2">
+                        <option value="Category One">Category One</option>
+                        <option value="Category One">Category One</option>
+                        <option value="Category One">Category One</option>
+                    </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl border-none placeholder-gray-900 text-sm px-4 py-2" placeholder="describe your idea"></textarea>
+                    </div>
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-600 w-5 transform -rotate-450" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                        </svg>
+                        <span class="ml-1">Attach</span></button>
+
+                        <button type="submit"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+                        >
+                        <span class="ml-1">Submit</span></button>
+                    </div>
+                </form>
+                </div>
             </div>
             <div  class="w-175">
                 <nav class="flex items-center justify-between text-xs">
@@ -71,6 +114,9 @@
                     {{$slot}}
                 </div>
             </div>
+            <!-- <div class="w-24">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci necessitatibus esse ea nostrum delectus quae impedit expedita reiciendis fugiat aliquam, quas hic nihil rerum, soluta culpa itaque laboriosam, voluptate ipsum.
+            </div> -->
         </main>
     </body>
 </html>
